c4e4da94b4dadb9f8a205f2012aba9f52e95a330
rkompella <ramk255@yahoo.com>
2015-07-06 22:25:32 -0400

    my first rally app

---
+++ App.js
@@ -0,0 +1,44 @@
+Ext.define('CustomApp', {
+    
+    extend: 'Rally.app.App',
+    
+    componentCls: 'app',
+    
+    
+     launch: function() {
+        
+       console.log("My first app, Woot!");
+       this._loadData();
+      },
+      
+ 
+      _loadData: function() {      
+         
+    var myStore = Ext.create('Rally.data.wsapi.Store', {
+             model: 'Task',
+        autoLoad: true,
+            listeners: {
+            load: function(myStore, MyData, success) {
+        console.log('Got data',myStore,MyData,success);
+                this._loadGrid(myStore);
+                
+            },
+        scope: this
+             },
+        fetch: ['FormattedID', 'Name','ToDo']
+        });
+     },
+     _loadGrid: function(myStorystore) {
+         var myGrid = Ext.create('Rally.ui.grid.Grid', {
+                    store: myStorystore,
+            columnCfgs: [
+            'FormattedID',
+            'Name',
+            'Todo'
+                 ]
+       });
+           this.add(myGrid);
+       console.log("what is this", this);
+      }
+});
+
